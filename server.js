9292066788a5bf30ba4b8c17992735ac1796e402
Sourav Mangla <mangla.sourav96@gmail.com>
2019-08-07 19:28:03 +0530

    layout

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
-const express = require("express");
-const app = express();
+const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
+const users = require('./routes/users')
+// const passport = require('passport')
+require('./models/dbConfig')
+require('./config/passport')
 
-app.listen(3000, () => {
-  console.log("server is running on 3000");
-});
+const port = process.env.PORT || 5000
+const app = express()
+
+
+//middlewares
+app.use(cors())
+app.use(express.json())
+app.use(morgan('dev'))
+
+//routes
+app.use('/users', users)
+
+app.listen(port, () => {
+  console.log(`server is running on ${port}`)
+})
